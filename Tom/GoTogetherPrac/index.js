a4f3e557ee08786df04014551610a0ffc86006da
Thomas <Thomas8241@gmail.com>
2023-09-08 10:56:17 -0500

    added unecesarry code to notes.md

--- Tom/GoTogetherPrac/index.js
+++ Tom/GoTogetherPrac/index.js
@@ -51,42 +51,6 @@ app.get("/createProfilePage", (req, res) => {
     res.render("createProfilePage.ejs")
 });
 
-// Another Test Route, more data entered, it worked!
-// app.get("/profileCreate", (req, res) => {
-//     const C_Profile = new CreateProfile({
-//         First_Name: 'Thomas',
-//         Last_Name: 'Smith',
-//         Birthday: '1994-01-25',
-//         Profile_Image: 'https://m.media-amazon.com/images/M/MV5BMTQ0NjgzNzcwNV5BMl5BanBnXkFtZTcwODExMDYxOQ@@._V1_.jpg',
-//         Age: 29,
-//         Gender: 'Male',
-//         Current_City: 'San Antonio',
-//         Hometown: 'Laredo',
-//         Profession: 'Programmer',
-//         Interests: 'Videogames, Godzilla, Lockpicking',
-//         Favorite_Movie: 'Titanic',
-//         Favorite_Artist: 'Joji',
-//         Favorite_Food: 'Enchiladas',
-//         Languages: 'English',
-//         Summary_Of_You: 'I am a nerdy guy who likes to program and play videogames!'
-//     })
-//     C_Profile.save();
-//     res.send(C_Profile)
-// })
-// This was a testing route to see if I could send mock data to the
-// database, it worked!
-// app.get("/firstLast", (req, res) => {
-//     const Fname_Lname = new FirstAndLastName({
-//         First_Name: '',
-//         Last_Name: ''
-//     })
-//     Fname_Lname.save();
-//     res.send(Fname_Lname)
-// })
-
-
-
-
 // READ
 // If you click the profile button option in the account dropbox,
 // this will trigger a route handler that displays your profile
--- Tom/GoTogetherPrac/notes.md
+++ Tom/GoTogetherPrac/notes.md
@@ -21,3 +21,30 @@ module.exports = mongoose.model("firstlastdata", FirstAndLastSchema);
 
 // You need this line in the index.js file to activate the model
  const FirstAndLastName = require('./models/FirstAndLastModel')
+
+
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Another Test Route, more data entered, it worked!
+// app.get("/profileCreate", (req, res) => {
+//     const C_Profile = new CreateProfile({
+//         First_Name: 'Thomas',
+//         Last_Name: 'Smith',
+//         Birthday: '1994-01-25',
+//         Profile_Image: 'https://m.media-amazon.com/images/M/MV5BMTQ0NjgzNzcwNV5BMl5BanBnXkFtZTcwODExMDYxOQ@@._V1_.jpg',
+//         Age: 29,
+//         Gender: 'Male',
+//         Current_City: 'San Antonio',
+//         Hometown: 'Laredo',
+//         Profession: 'Programmer',
+//         Interests: 'Videogames, Godzilla, Lockpicking',
+//         Favorite_Movie: 'Titanic',
+//         Favorite_Artist: 'Joji',
+//         Favorite_Food: 'Enchiladas',
+//         Languages: 'English',
+//         Summary_Of_You: 'I am a nerdy guy who likes to program and play videogames!'
+//     })
+//     C_Profile.save();
+//     res.send(C_Profile)
+// })
